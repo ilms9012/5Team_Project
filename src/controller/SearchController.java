a249b081280829a9969ce362c53266424c2572eb
ilms9012 <31472934+ilms9012@users.noreply.github.com>
2017-12-04 11:38:48 +0900

    asda
    asd

--- src/controller/SearchController.java
+++ src/controller/SearchController.java
@@ -21,12 +21,29 @@ public class SearchController {
 		ModelAndView mv = new ModelAndView();
 		System.out.println(nickname);
 		List<StatVO> result = service.statSearch(nickname);
-		if(result==null) {
+		int asMatches = 0;
+		int krjpMatches = 0;
+		int serverMode1 = 0;
+		int serverMode2 = 3;
+		for(int i = 0; i<3 ; i++) {
+			int sum = result.get(i).getWins()+result.get(i).getLosses();
+			int sum2 = result.get(i+3).getWins()+result.get(i+3).getLosses();
+			asMatches = asMatches+sum;
+			krjpMatches = krjpMatches+sum2;
+		}
+		if(asMatches+krjpMatches==0) {
 			mv.addObject("statInfo","사용자 정보가 없습니다.");
-			mv.setViewName("search_result");
-		}else {
-			
+			mv.setViewName("search_result_fail");
+		}else if(asMatches>krjpMatches){
 			mv.addObject("statInfo",result);
+			mv.addObject("serverMode",serverMode1);
+			mv.setViewName("search_result");
+		}else if(asMatches<krjpMatches) {
+			mv.addObject("statInfo",result);
+			mv.addObject("serverMode",serverMode2);
+		}else {
+			mv.addObject("statInfo",result);
+			mv.addObject("serverMode",serverMode1);
 			mv.setViewName("search_result");
 		}
 		return mv;
@@ -60,5 +77,10 @@ public class SearchController {
 		mv.setViewName("compare_result");
 		
 		return mv;
+	}
+	@RequestMapping("/DBSearch.do")
+	public ModelAndView DBSearch(String nickname,int serverMode) {
+		ModelAndView mv = new ModelAndView();
+		List<StatVO> result = service.DBSearch(nickname,serverMode);
 	}
 }
